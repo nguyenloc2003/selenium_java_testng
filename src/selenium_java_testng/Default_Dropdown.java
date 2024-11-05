501577baa1b32c082d30d7d7703211e412ead8f2
nguyenloc2003 <nguyenloc31102003@gmail.com>
2024-11-05 13:55:42 +0700

    Default_Dropdown

--- src/selenium_java_testng/Default_Dropdown.java
+++ src/selenium_java_testng/Default_Dropdown.java
@@ -1,26 +1,95 @@
 package selenium_java_testng;
 
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+
+import java.util.Random;
 
 public class Default_Dropdown {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
+	String firstName, lastName, emailAddress, companyName, password, day, month, year;
 	
 	@BeforeClass
 	public void beforeClass() {
-		
+		if (osName.contains("MAC OS")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		} else {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		}
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		firstName = "Loc";
+		lastName = "Nguyen";
+		emailAddress = "nguyenloc" + getRandomNumber() + "@gmail.com";
+		companyName = "ABCD";
+		password = "123456";
+		day = "20";
+		month = "September";
+		year = "2024";
 	}
 	
 	@Test
+	public void Register_New_Account() {
+		driver.get("https://demo.nopcommerce.com/register");
+		
+		driver.findElement(By.xpath("//input[@id='FirstName']")).sendKeys(firstName);
+		driver.findElement(By.xpath("//input[@id='LastName']")).sendKeys(lastName);
+		new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthDay']"))).selectByVisibleText(day);
+		new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']"))).selectByVisibleText(month);
+		new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthYear']"))).selectByVisibleText(year);
+		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(emailAddress);
+		driver.findElement(By.xpath("//input[@name='Company']")).sendKeys(companyName);
+		driver.findElement(By.xpath("//input[@name='Password']")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@name='ConfirmPassword']")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@name='register-button']")).click();
+		
+		sleepInSecond(10);
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='result']")).getText(), "Your registration completed");
+		
+		driver.findElement(By.xpath("//a[@class='ico-account']")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='FirstName']")).getAttribute("value"), firstName);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='LastName']")).getAttribute("value"), lastName);
+		Assert.assertEquals(new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthDay']"))).getFirstSelectedOption().getText(), day);
+		Assert.assertEquals(new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']"))).getFirstSelectedOption().getText(), month);
+		Assert.assertEquals(new Select(driver.findElement(By.xpath("//select[@name='DateOfBirthYear']"))).getFirstSelectedOption().getText(), year);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='Email']")).getAttribute("value"), emailAddress);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='Company']")).getAttribute("value"), companyName);
+	}
+	
+	// @Test
+	public void Add_Address() {
+		
+	}
 	
 	
+	public int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(99999);
+	}
+	
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	@AfterClass
 	public void afterClass() {
-		
+		// driver.quit();
 	}
 }
--- src/selenium_java_testng/Random.java
+++ src/selenium_java_testng/Random.java
@@ -1,18 +1,27 @@
 package selenium_java_testng;
 
-public class Random {
-	public static void main(String[] args) {
-		java.util.Random rand = new java.util.Random(); 
-		System.out.println(rand.nextFloat());
-		System.out.println(rand.nextDouble());
-		System.out.println(rand.nextInt());
-		System.out.println(rand.nextInt(999999));
-		System.out.println(rand.nextInt(99999));
-		System.out.println("automation" + rand.nextInt(9999) + "@gmail.com");
-		System.out.println(rand.nextInt(999));
-		System.out.println(rand.nextLong());
-	}
 
+public class Random {
+//	public static void main(String[] args) {
+//		Random rand = new Random(); 
+//		System.out.println(rand.nextFloat());
+//		System.out.println(rand.nextDouble());
+//		System.out.println(rand.nextInt());
+//		System.out.println(rand.nextInt(999999));
+//		System.out.println(rand.nextInt(99999));
+//		System.out.println("automation" + rand.nextInt(9999) + "@gmail.com");
+//		System.out.println(rand.nextInt(999));
+//		System.out.println(rand.nextLong());
+//	}
 	
+	public static void main(String[] args) {
+		System.out.println("nguyenloc" + getRandomNumber() + "@gmail.com");
+	}
+	
+	
+	public static int getRandomNumber() {
+		java.util.Random rand = new java.util.Random();
+		return rand.nextInt(99999);
+	}
 	
 }
