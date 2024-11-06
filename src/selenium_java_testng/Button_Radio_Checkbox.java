92b59d2d974a18d376c47d8fe6b2142954ee0d66
nguyenloc2003 <nguyenloc31102003@gmail.com>
2024-11-06 15:27:48 +0700

    Button_Radio_Checkbox

--- src/selenium_java_testng/Button_Radio_Checkbox.java
+++ src/selenium_java_testng/Button_Radio_Checkbox.java
@@ -1,10 +1,13 @@
 package selenium_java_testng;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,7 +16,7 @@ public class Button_Radio_Checkbox {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
-	
+
 	@BeforeClass
 	public void beforeClass() {
 		if (osName.contains("MAC OS")) {
@@ -25,17 +28,72 @@ public class Button_Radio_Checkbox {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
-	
-	@Test 
-	public void TC_01_Button() {
-		driver.get("https://www.fahasa.com/customer/account/create");
-		
-		driver.findElement(By.xpath("//li[@class='popup-login-tab-item popup-login-tab-login active']")).click();
-		
-		
-		
+
+	@Test
+	public void Default_Checkbox_Radio_Single() {
+		driver.get("https://automationfc.github.io/multiple-fields/");
+
+		driver.findElement(By.xpath("//label[contains(text(), 'Hepatitis')]/preceding-sibling::input")).click();
+
+		driver.findElement(By.xpath("//label[contains(text(),'1-2 cups/day')]/preceding-sibling::input")).click();
+
+		Assert.assertTrue(driver
+				.findElement(By.xpath("//label[contains(text(), 'Hepatitis')]/preceding-sibling::input")).isSelected());
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//label[contains(text(),'1-2 cups/day')]/preceding-sibling::input"))
+						.isSelected());
+
+		driver.findElement(By.xpath("//label[contains(text(), 'Hepatitis')]/preceding-sibling::input")).click();
+
+		Assert.assertFalse(driver
+				.findElement(By.xpath("//label[contains(text(), 'Hepatitis')]/preceding-sibling::input")).isSelected());
+
+		driver.findElement(By.xpath("//label[contains(text(), 'Hepatitis')]/preceding-sibling::input")).click();
+
+		Assert.assertTrue(driver
+				.findElement(By.xpath("//label[contains(text(), 'Hepatitis')]/preceding-sibling::input")).isSelected());
+
 	}
-	
+
+	@Test
+	public void Default_Checkbox_Multiple() {
+		driver.get("https://automationfc.github.io/multiple-fields/");
+
+		List<WebElement> allCheckboxes = driver.findElements(By.cssSelector("input.form-checkbox"));
+
+		for (WebElement checkbox : allCheckboxes) {
+			checkbox.click();
+		}
+
+		for (WebElement checkbox : allCheckboxes) {
+			Assert.assertTrue(checkbox.isSelected());
+		}
+
+		for (WebElement checkbox : allCheckboxes) {
+			if (checkbox.getAttribute("value").equals("Asthma")) {
+				checkbox.click();
+			}
+		}
+
+	}
+
+	@Test
+	public void Default_Checkbox() {
+		driver.get("https://demos.telerik.com/kendo-ui/checkbox/index");
+		
+		if (!driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).isSelected()) {
+			driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).click();
+		}
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).isSelected());
+		
+		if (driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).isSelected()) {
+			driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).click();
+		}
+		
+		Assert.assertFalse(driver.findElement(By.xpath("//label[text()='Dual-zone air conditioning']/preceding-sibling::span/input")).isSelected());
+	}
+
 	public void sleepInSecond(long timeInSecond) {
 		try {
 			Thread.sleep(timeInSecond * 1000);
@@ -44,9 +102,8 @@ public class Button_Radio_Checkbox {
 		}
 	}
 
-	
 	@AfterClass
 	public void afterClass() {
-
+//		driver.quit();
 	}
 }
